6366b8a8cf3f0fc8c9fb98d62a49735e1fca33b7
eduardo.jardim <eduardo.salomao.584@gmail.com>
2024-06-18 20:28:09 -0300

    criando classe Livro

---
+++ src/com/br/biblioteca/model/Livro.java
@@ -0,0 +1,55 @@
+package com.br.biblioteca.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Livro {
+	@SerializedName("titulo")
+	private String titulo;
+	@SerializedName("autor")
+	private String autor;
+	@SerializedName("genero")
+	private String genero;
+	@SerializedName("genero")
+	private int exemplares;
+	
+	public Livro(String titulo, String autor, String genero, int exemplares) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.genero = genero;
+		this.exemplares = exemplares;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getExemplares() {
+		return exemplares;
+	}
+
+	public void setExemplares(int exemplares) {
+		this.exemplares = exemplares;
+	}
+	
+	
+}
