a618c6da1f7184b73a0ad0f5b3c5eb98b29b5c81
eduardo.jardim <eduardo.salomao.584@gmail.com>
2024-06-19 23:59:57 -0300

    alterando função de alterar para verificar se o livro está no json

--- src/com/br/biblioteca/menu/Menu.java
+++ src/com/br/biblioteca/menu/Menu.java
@@ -3,13 +3,14 @@ package com.br.biblioteca.menu;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Menu {
 
+public class Menu {
+	// Método para exibir o menu e obter a opção do usuário
 	public int exibirMenu() {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // Cria um objeto Scanner para ler a entrada do usuário
 		int opcao = -1; // Inicializa com um valor inválido para garantir que o loop funcione
 
-		while (true) {
+		while (true) {  // Loop será interrompido apenas quando uma entrada válida for recebida
 			System.out.println(" Biblioteca de livros");
 			System.out.println("-----------------------");
 			System.out.println("| Operações:          |");
@@ -22,11 +23,11 @@ public class Menu {
 
 			System.out.printf("Sua opção: ");
 			try {
-				opcao = sc.nextInt();
+				opcao = sc.nextInt(); // Tenta ler a próxima entrada como um inteiro
 				break; // Se a leitura for bem-sucedida, sai do loop
 			} catch (InputMismatchException e) {
 				System.out.println("Entrada inválida. Por favor, insira um número (0-4).");
-				sc.next(); // Consome a entrada inválida
+				sc.next(); // Consome a entrada inválida para evitar um loop infinito
 			}
 		}
 
