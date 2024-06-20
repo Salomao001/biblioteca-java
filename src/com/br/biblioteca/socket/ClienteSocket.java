e3b2e6ad4af9c5e3857646d69908a86e5517bef2
Cecilia Cortes Lopes <ceciliacorteslopes@gmail.com>
2024-06-19 23:41:52 -0300

    altera clientesocket

--- src/com/br/biblioteca/socket/ClienteSocket.java
+++ src/com/br/biblioteca/socket/ClienteSocket.java
@@ -14,20 +14,20 @@ import com.br.biblioteca.menu.Menu;
 public class ClienteSocket {
 
 	public static void main(String[] args) {
-		ClienteFunctions clienteFunctions = new ClienteFunctions();
+		ClienteFunctions clienteFunctions = new ClienteFunctions(); // Cria uma instância de ClienteFunctions para manipular as operações do cliente
 
 		try {
-			Socket socket = new Socket("localhost", 12345);
+			Socket socket = new Socket("localhost", 12345); // Cria um socket para se conectar ao servidor na porta 12345 do localhost
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Cria um BufferedReader para ler dados do servidor
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // Cria um BufferedWriter para enviar dados ao servidor
 
 			int opcao = 0;
 			do {
-				opcao = new Menu().exibirMenu();
-				out.write(String.valueOf(opcao));
-				out.newLine(); // Adiciona uma nova linha para garantir que o dado seja lido corretamente pelo servidor
-				out.flush();
+				opcao = new Menu().exibirMenu();// Exibe o menu e obtém a opção do usuário
+				out.write(opcao);
+
+				out.flush(); // Garante que os dados sejam enviados imediatamente
 
 				switch (opcao) {
 					case 1:
@@ -52,11 +52,11 @@ public class ClienteSocket {
 
 			} while (opcao != 0);
 
-			socket.close();
+			socket.close(); // Fecha o socket após o término da aplicação
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			e.printStackTrace(); // Imprime a pilha de erros se o host for desconhecido
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); // Imprime a pilha de erros para exceções de E/S
 		}
 
 	}
